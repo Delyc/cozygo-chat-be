e747d5e8f382a41338f34ec18e91b8a7f58db09a
delyc <delyce35@gmail.com>
2024-03-15 14:38:23 +0200

    feat: adding user details

--- models/user.model.js
+++ models/user.model.js
@@ -4,18 +4,39 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+
   email: {
     type: String,
     required: true,
   },
-  password:{
-    type:String,
+  password: {
+    type: String,
     required: true,
   },
   // email:{
 
   // }
-}, {timestamps: true});
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 export default User
